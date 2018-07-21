95a9b8544544007e168ab2f82664a7e55ffb1380
Jennifer Meara <jenniferlmeara@gmail.com>
2018-07-21 05:00:20 +0000

    Complete model.py file

--- model.py
+++ model.py
@@ -12,20 +12,39 @@ db = SQLAlchemy()
 ##############################################################################
 # Part 1: Compose ORM
 
-# class Brand(db.Model):
-#     """Car brand."""
+class Brand(db.Model):
+    """Car brand."""
 
-#     __tablename__ = "brands"
+    __tablename__ = "brands"
 
-#     pass
+    brand_id = db.Column(db.String(5), primary_key=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    founded = db.Column(db.Integer, nullable=True)
+    headquarters = db.Column(db.String(50), nullable=True)
+    discontinued = db.Column(db.Integer, nullable=True)
+
+    models = db.relationship('Model', backref=db.backref('ratings'))
+
+    def __repr__(self):
+        """Helpful representation when printing"""
+
+        return f"<Brand brand_id={self.brand_id} name={self.name}>"
 
 
-# class Model(db.Model):
-#     """Car model."""
+class Model(db.Model):
+    """Car model."""
 
-#     __tablename__ = "models"
+    __tablename__ = "models"
 
-#     pass
+    model_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    year = db.Column(db.Integer, nullable=False)
+    brand_id = db.Column(db.String(5), db.ForeignKey('brands.brand_id'), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        """Helpful representation when printing."""
+
+        return f"Model model_id={self.model_id} name={self.name}>"
 
 # End Part 1
 
